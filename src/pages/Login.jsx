bf9b7f0fd74954f062fed0dc3ef0fbc322081b84
Evinich <nnamdiezenwegbu@gmail.com>
2024-10-17 17:31:39 +1000

    fixing error on homepage and installing react icon

---
+++ src/pages/Login.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import DarkBG from '../components/DarkBG'
+import FormComponent from '../components/FormComponent'
+import Footer from '../components/Footer'
+
+export default function Login() {
+  return (
+    <div>
+        <Navbar />
+        <DarkBG text='My Account'> 
+            <p>Home  /  My Account</p>
+        </DarkBG>
+        <FormComponent />
+        <Footer />
+    </div>
+  )
+}
