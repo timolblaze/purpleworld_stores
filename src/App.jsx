378c58e5d7a0a6cae65cd904a612eee48608cd9e
Uchenna Obi <greenfieldsolutions32@gmail.com>
2024-11-07 11:22:35 +1000

    worked on the app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,22 +6,32 @@ import ProductDetails from "./pages/ProductDetails/ProductDetails";
 import Shop from "./pages/Shop/Shop";
 import ShoppingCart from "./pages/ShoppingCart/ShoppingCart";
 import ProductsLayout from "./components/ProductsLayout";
+import ProductsList from "./components/ProductsList";
+import GroceriesList from "./components/GroceriesList";
+import SkinCareList from "./components/SkinCareList"
+import CartProvider from "./contexts/CartProvider";
 
 function App() {
   return (
+    <CartProvider>
       <BrowserRouter>
         <Routes>
           <Route index element={<Homepage />} />
           <Route path="/login" element={<Login />} />
 
           <Route element={<ProductsLayout />}>
-              <Route path="/shop" element={<Shop />} />
+              <Route path="/shop" element={<Shop />}> 
+                <Route index element={<ProductsList /> } />
+                <Route path="groceries" element={ <GroceriesList /> } />
+                <Route path="skincare" element={ <SkinCareList /> } />
+              </Route>
               <Route path="shop/:productId" element={<ProductDetails />} />
           </Route>
             
           <Route path="/cart" element={<ShoppingCart />} />
         </Routes>
       </BrowserRouter>
+    </CartProvider>
   );
 }
 
