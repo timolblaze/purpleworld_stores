9ec2af07e02ae0f5074a6db120d01477c689a8f0
Uchenna Obi <greenfieldsolutions32@gmail.com>
2024-11-11 19:44:01 +1000

    mock.js updated

--- src/components/mock.js
+++ src/components/mock.js
@@ -15,4 +15,67 @@ export const cartTotals = {
     subtotal: 8500,
     total: 8500,
   };
+
+  export const countries = [
+    { code: "NGN", name: "Nigeria" },
+    { code: "US", name: "United States" },
+    { code: "CA", name: "Canada" },
+    { code: "GB", name: "United Kingdom" },
+    { code: "AU", name: "Australia" },
+    { code: "DE", name: "Germany" },
+    { code: "FR", name: "France" },
+    { code: "IN", name: "India" },
+    { code: "JP", name: "Japan" },
+    { code: "BR", name: "Brazil" },
+    { code: "ZA", name: "South Africa" },
+  ];
+
+  export const Housing = [
+    "Apartment", "Suite", "Unit" ,
+  ];
   
+  export const statesInNigeria = [
+    "Abia", "Adamawa", "Akwa Ibom", "Anambra", "Bauchi", "Bayelsa", "Benue", "Borno", 
+    "Cross River", "Delta", "Ebonyi", "Edo", "Ekiti", "Enugu", "Gombe", "Imo", 
+    "Jigawa", "Kaduna", "Kano", "Katsina", "Kebbi", "Kogi", "Kwara", 
+    "Lagos", "Nasarawa", "Niger", "Ogun", "Ondo", "Osun", "Oyo", 
+    "Plateau", "Rivers", "Sokoto", "Taraba", "Yobe", "Zamfara", "FCT"
+  ];
+  
+  const mockOrderData = {
+    id: "123456",
+    products: [
+      {
+        name: "Pringles Original",
+        quantity: "12 x 1",
+        price: 8500,
+      },
+    ],
+    subtotal: 8500,
+    total: 8500,
+  };
+  
+  export default mockOrderData;
+  
+
+  export const fakeData = {
+    orderInfo: [
+      { label: 'Order number', value: '16105' },
+      { label: 'Date', value: 'June 19, 2024' },
+      { label: 'Total', value: '₦9,925.00' },
+      { label: 'Payment method', value: 'Direct bank transfer' },
+    ],
+    bankDetails: {
+      name: 'Ojieog Munachiso Elvis',
+      bank: 'GT Bank',
+      accountNumber: '0472649782',
+    },
+    orderDetails: [
+      { label: 'Pringles Original x12 × 1', amount: '₦8,500.00' },
+      { label: 'Subtotal', amount: '₦8,500.00' },
+      { label: 'Shipping', amount: '₦1,425.00 via Flat rate' },
+      { label: 'Payment method', amount: 'Direct bank transfer' },
+      { label: 'Total', amount: '₦9,925.00' },
+    ],
+    note: 'just for you',
+  };
