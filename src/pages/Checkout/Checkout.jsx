fc5b9a9a6947c357cd20d1eb94d8c7bd6eea4c17
Uchenna Obi <greenfieldsolutions32@gmail.com>
2024-11-15 16:50:51 +1000

    Changed country to Nigeria

--- src/pages/Checkout/Checkout.jsx
+++ src/pages/Checkout/Checkout.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from "react";
-import { countries, Housing, statesInNigeria, statesInAustralia} from "../../components/mock";
+import { countries, Housing, statesInNigeria, statesInNigeria} from "../../components/mock";
 import { Link, useNavigate } from "react-router-dom";
 import Navbar from "../../components/Navbar";
 import Process from "../../components/Process/Process";
@@ -311,7 +311,7 @@ const Checkout = () => {
                   required
                 >
                   <option value="">Select a state</option>
-                  {statesInAustralia.map((state, index) => (
+                  {statesInNigeria.map((state, index) => (
                     <option key={index} value={state}>
                       {state}
                     </option>
